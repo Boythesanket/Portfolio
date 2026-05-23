b3fb2ed557a11c1152c1de2badab29d2b15c7e45
Boythesanket <opbanditmarco@gmail.com>
2026-05-23 07:07:33 +0530

    portfolio-new

--- backend/utils/nodeMailer.js
+++ backend/utils/nodeMailer.js
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import nodemailer from "nodemailer";
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
